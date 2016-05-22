136a996e48e6d0a6ccfcd0a708f10ee6d59a962b
hhsieh <hhsieh@users.noreply.github.com>
2016-05-22 16:24:22 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,14 +1,20 @@
 shinyUI(pageWithSidebar(
   headerPanel('Unlimited networks'),
   sidebarPanel(
-    sliderInput('thresh', 'Threshold distance (m)', 23,
+    sliderInput('thresh', 'Threshold distance (m)', 15,
                 min = 8, max = 45, step = 0.1)
   ),
   mainPanel(
     tabsetPanel(type = "tabs",
-                tabPanel("Plots", h4("Upper plot controls lower plot - brush to zoom in"), plotOutput('plot1', brush = brushOpts(id = "plot1_brush", resetOnNew = TRUE)),
+                tabPanel("Edges Table", h4("Edges of the network"), tableOutput('table1')),
+                tabPanel("Nodes Table", h4("Nodes of the network"), tableOutput('table2')),
+                tabPanel("Network Plots", h4("Upper plot controls lower plot - brush to zoom in"), plotOutput('plot1', brush = brushOpts(id = "plot1_brush", resetOnNew = TRUE)),
                          "", plotOutput('plot2')),
-                tabPanel("Table", tableOutput('table1'))
+                tabPanel("Summary Plots", plotOutput('plot3'),
+                         "", plotOutput('plot4'),
+                         "", plotOutput('plot5'),
+                         "", plotOutput('plot6'))
+                
     )
   )
 ))
